6d5224ace1cd4a14cc1577e28b8ffffded8f6f8b
AlsoSanidhya <waynesfr@gmail.com>
2025-11-05 19:50:56 +0530

    Q79: Perform diagonal traversal of a matrix.
    /*
    Sample Test Cases:
    Input 1:
    3 3
    1 2 3
    4 5 6
    7 8 9
    Output 1:
    1 2 4 7 5 3 6 8 9
    
    */

---
+++ Q79.c
@@ -0,0 +1,37 @@
+//Q79: Perform diagonal traversal of a matrix.
+
+/*
+Sample Test Cases:
+Input 1:
+3 3
+1 2 3
+4 5 6
+7 8 9
+Output 1:
+1 2 4 7 5 3 6 8 9
+
+*/
+#include <stdio.h>
+void diagonalTraversal(int rows, int cols) {
+    int matrix[rows][cols];
+
+    // Read the matrix elements
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    // Perform diagonal traversal
+    for (int d = 0; d < rows + cols - 1; d++) {
+        int r = (d < cols) ? 0 : d - cols + 1;
+        int c = (d < cols) ? d : cols - 1;
+
+        while (r < rows && c >= 0) {
+            printf("%d ", matrix[r][c]);
+            r++;
+            c--;
+        }
+    }
+    printf("\n");
+}   
