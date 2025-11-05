a8e0feb500595195d27ad4592da6eef51e10d02c
AlsoSanidhya <waynesfr@gmail.com>
2025-11-05 22:06:31 +0530

    Q49: Write a program to print the following pattern: 5 45 345 2345 12345
    /*
    Sample Test Cases:
    Input 1:
    
    Output 1:
    5
    45
    345
    2345
    12345
    
    */

--- Q49.c
+++ Q49.c
@@ -17,3 +17,22 @@ Output 1:
 12345
 
 */
+#include <stdio.h>
+int main() {
+    int rows = 5;
+
+    // Print the pattern
+    for (int i = 1; i <= rows; i++) {
+        // Print leading spaces
+        for (int j = 1; j <= rows - i; j++) {
+            printf(" ");
+        }
+        // Print numbers
+        for (int k = rows - i + 1; k <= rows; k++) {
+            printf("%d", k);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
