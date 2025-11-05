d97420f15f71569f4edf540d7aa09baef956bac0
AlsoSanidhya <waynesfr@gmail.com>
2025-11-05 19:45:18 +0530

    Read and print a matrix

---
+++ Q71.c
@@ -0,0 +1,32 @@
+//Q71: Read and print a matrix.
+
+/*
+Sample Test Cases:
+Input 1:
+2 2
+1 2
+3 4
+Output 1:
+1 2
+3 4
+
+*/
+#include <stdio.h>
+void readAndPrintMatrix(int rows, int cols) {
+    int matrix[rows][cols];
+
+    // Read the matrix elements
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    // Print the matrix elements
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
