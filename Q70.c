b64058735369ea6747f6141d770b3fdfdcdba621
AlsoSanidhya <waynesfr@gmail.com>
2025-11-05 22:04:30 +0530

    Q70: Rotate an array to the right by k positions.
    /*
    Sample Test Cases:
    Input 1:
    5
    1 2 3 4 5
    2
    Output 1:
    4 5 1 2 3
    
    */

--- Q70.c
+++ Q70.c
@@ -30,3 +30,35 @@ void rotateArray(int arr[], int n, int k) {
         arr[i] = temp[i];
     }
 }
+int main() {
+    int n;
+
+    // Input size of the array
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    // Input elements of the array
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int k;
+    // Input number of positions to rotate
+    printf("Enter number of positions to rotate: ");
+    scanf("%d", &k);
+
+    // Rotate the array
+    rotateArray(arr, n, k);
+
+    // Print the rotated array
+    printf("Rotated array:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
