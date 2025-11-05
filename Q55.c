8da57e39c1130ceee103f33629c94b262e63e65e
AlsoSanidhya <waynesfr@gmail.com>
2025-11-05 07:16:37 +0530

    Write a program to print all the prime numbers from 1 to n.

---
+++ Q55.c
@@ -0,0 +1,27 @@
+//Q55: Write a program to print all the prime numbers from 1 to n.
+
+/*
+Sample Test Cases:
+Input 1:
+10
+Output 1:
+2 3 5 7
+
+Input 2:
+20
+Output 2:
+2 3 5 7 11 13 17 19
+
+*/
+#include <stdio.h>
+int isPrime(int num) {
+    if (num <= 1) {
+        return 0; // Not prime
+    }
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) {
+            return 0; // Not prime
+        }
+    }
+    return 1; // Prime
+}
